ed8ae7e7e960342d2b8ec4014d6cc39eb0e5c91b
Axel Segundo <axeljasegundo@gmail.com>
2020-09-11 11:45:49 -0500

    Baraja

--- assests/js/juego.js
+++ assests/js/juego.js
@@ -5,29 +5,29 @@
  * 2S = two of spades (picas)
  */
 
- let deck = [];
- const tipos = ['C', 'D', 'H', 'S'];
- const especiales = ['A', 'J', 'Q', 'K'];
+let deck         = [];
+const tipos      = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
 
- const crearDeck = () => {
+const clearDeck = () => {
 
-    for(let i = 2; i<=10; i++) {
-        for( let tipo of tipos) {
-            deck.push( i + tipo);
+    for( let i = 2; i <=10; i++ ) {
+        for( let tipo of tipos ) {
+            deck.push( i + tipo );
         }
     }
 
-    for(let tipo of tipos) {
-        for(let esp of especiales){
-            deck.push(esp + tipo);
+    for( let tipo of tipos ) {
+        for( let esp of especiales ) {
+            deck.push( esp + tipo );
         }
     }
 
-    console.log(deck);
-
-    deck = _.shuffle ( deck );
-    console.log(deck);
+    console.log( deck );
+    deck = _.shuffle( deck );
+    console.log( deck );
     return deck;
- }
 
- crearDeck ();
+} 
+
+clearDeck(); 
