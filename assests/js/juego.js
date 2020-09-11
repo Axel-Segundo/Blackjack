13fc317943e76d9dee67d1ed3e205419e4845cdc
Axel Segundo <axeljasegundo@gmail.com>
2020-09-11 12:13:50 -0500

    Obtener carta

--- assests/js/juego.js
+++ assests/js/juego.js
@@ -8,10 +8,16 @@ let deck         = [];
 const tipos      = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+let puntosJugador = 0,
+    puntosCosmputadora = 0;
+
+// Referencias
+const btnPedir = document.querySelector('#btnPedir')
+
+const puntosHTML = document.querySelectorAll('small');
 
 // crear deck
 const clearDeck = () => {
-
     for( let i = 2; i <=10; i++ ) {
         for( let tipo of tipos ) {
             deck.push( i + tipo );
@@ -23,40 +29,39 @@ const clearDeck = () => {
         }
     }
 
-    console.log( deck );
     deck = _.shuffle( deck );
-    console.log( deck );
     return deck;
 
 } 
-
-clearDeck(); 
 clearDeck();
-
 //tomar carta
 const pedirCarta = () => {
-
     if ( deck.length === 0 ) {
         throw 'No hay mas cartas en el deck';
     }
 
-
-    const carta = deck.pop();
-
-    console.log(deck);
-    console.log(carta); //carta que baja
     return carta;
 }
 
 //pedirCarta();
-
 const valorCarta = ( carta ) => {
 
     const valor = carta.substring(0, carta.length - 1);
-    return ( isNAN( valor ) ) ?
+    return ( isNaN( valor ) ) ?
             ( valor === 'A' ) ? 11 : 10
             : valor * 1;    
 }
 
-const valor = valorCarta( pedirCarta() );
-console.log({ valor });
+
+
+
+// Eventos
+btnPedir.addEventListener('click', () => {
+
+    const carta = pedirCarta();
+
+    puntosJugador = puntosJugador + valorCarta( carta );
+    puntosHTML[0].innerText = puntosJugador
+
+
+}); 
