220b5af00b9c3258797301322e17e120488dc15f
Axel Segundo <axeljasegundo@gmail.com>
2020-09-11 11:47:15 -0500

    Pedir carta

--- assests/js/juego.js
+++ assests/js/juego.js
@@ -4,11 +4,12 @@
  * 2H = two of hearts (corazon)
  * 2S = two of spades (picas)
  */
-
 let deck         = [];
 const tipos      = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+
+// crear deck
 const clearDeck = () => {
 
     for( let i = 2; i <=10; i++ ) {
@@ -16,7 +17,6 @@ const clearDeck = () => {
             deck.push( i + tipo );
         }
     }
-
     for( let tipo of tipos ) {
         for( let esp of especiales ) {
             deck.push( esp + tipo );
@@ -31,3 +31,21 @@ const clearDeck = () => {
 } 
 
 clearDeck(); 
+clearDeck();
+
+//tomar carta
+const pedirCarta = () => {
+
+    if ( deck.length === 0 ) {
+        throw 'No hay mas cartas en el deck';
+    }
+
+
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta); //carta que baja
+    return carta;
+}
+
+//pedirCarta();
